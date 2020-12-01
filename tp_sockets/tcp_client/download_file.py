ae0c3de69ad9438e1184e7ebe0cbb6b1c6e06c48
Tomas Franco <tomasnfranco@gmail.com>
2020-12-01 01:05:47 -0300

    Preliminary version of the download file feature

--- tp_sockets/tcp_client/download_file.py
+++ tp_sockets/tcp_client/download_file.py
@@ -1,4 +1,30 @@
+import socket
+from constants import *
+
+
 def download_file(server_address, name, dst):
-  # TODO: Implementar TCP download_file client
-  print('TCP: download_file({}, {}, {})'.format(server_address, name, dst))
-  pass
+    print('TCP: download_file({}, {}, {})'.format(server_address, name, dst))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+
+    message = '{},{}'.format(OP_CODE_DOWNLOAD, name)
+    sock.sendall(message.encode('utf-8'))
+
+    size = int(sock.recv(MESSAGE_SIZE).decode())
+    print("Received file size: {}".format(size))
+
+    fp = open(dst, "wb")
+
+    received = 0
+    while received < size:
+        chunk = sock.recv(MESSAGE_SIZE)
+        received += len(chunk)
+        fp.write(chunk)
+
+    end_transfer(fp, sock)
+
+
+def end_transfer(fp, sock):
+    fp.close()
+    sock.close()
--- tp_sockets/tcp_server/start_server.py
+++ tp_sockets/tcp_server/start_server.py
@@ -1,4 +1,64 @@
+import socket
+import os
+from constants import *
+
+
+def create_socket(server_address):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(server_address)
+    return sock
+
+
+def close_socket(sock):
+    sock.close()
+
+
 def start_server(server_address, storage_dir):
-  # TODO: Implementar TCP server
-  print('TCP: start_server({}, {})'.format(server_address, storage_dir))
-  pass
+    print('TCP: start_server({}, {})'.format(server_address, storage_dir))
+
+    sock = create_socket(server_address)
+    sock.listen()
+
+    while True:
+        connection, address = sock.accept()
+        if not connection:
+            return close_socket(sock)
+
+        print("Connection from {}".format(address))
+
+        parsed_response = connection.recv(MESSAGE_SIZE).decode().split(',')
+        op_code = int(parsed_response[0])
+        file_name = parsed_response[1]
+
+        full_file_name = "{}/{}".format(storage_dir, file_name)
+        if op_code == OP_CODE_DOWNLOAD:
+            start_download(full_file_name, connection)
+        elif op_code == OP_CODE_UPLOAD:
+            start_upload(full_file_name, connection)
+
+
+def start_download(file_name, connection):
+    fp = open(file_name, "rb")
+    size = os.path.getsize(file_name)
+
+    print("Sent file size: {}".format(size))
+
+    connection.send(str(size).encode())
+
+    while True:
+        chunk = fp.read(MESSAGE_SIZE)
+        if not chunk:
+            break
+        connection.send(chunk)
+
+    end_transfer(fp, connection)
+
+
+def start_upload(connection, storage_dir):
+    pass
+
+
+def end_transfer(fp, connection):
+    fp.close()
+    connection.close()
+
